c7b2944f0d4ba27cd6c75725f3409b648bd06971
sk49g74qf4-lgtm <sk49g74qf4@privaterelay.appleid.com>
2026-05-28 12:56:59 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -18,10 +18,10 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "7952634181:AAFtvSN6vrzJzfuwrejQxVqEIqD4lMC9wv8"
+BOT_TOKEN = "8877747207:AAEdZgbR-jQXkcYJdK2euIkqtS-idW8pUAU"
 YML_FILE_PATH = ".github/workflows/main.yml"
 BINARY_FILE_NAME = "soul"
-ADMIN_IDS = [1600832237, 1600832237]
+ADMIN_IDS = [1600832237, 6037730087]
 
 # Conversation states
 WAITING_FOR_BINARY = 1
